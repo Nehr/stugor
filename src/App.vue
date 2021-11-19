6c8c52bdf92fbae1597b0d5e14630e0f4dd0b144
Thom <thom@websquid.se>
2021-11-19 18:11:07 +0100

    Initial push

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <Navigation />
+  <main>
+    <router-view />
+  </main>
 </template>
 
+<script>
+import Navigation from './components/Navigation.vue';
+
+export default {
+  components: { Navigation },
+  props: {},
+  setup(props) {
+    console.log(props);
+
+    return {};
+  },
+};
+</script>
+
 <style lang="scss">
+body,
+html {
+  font-size: 16px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: $text-color;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+main {
+  padding-top: $header--height;
 }
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav class="nav">
+    <div class="nav__inner">
+      <div class="nav__inner--left">
+        <router-link to="/">[Stugor Logo]</router-link>
+      </div>
+      <div class="nav__inner--right">
+        <router-link to="/about">Användare</router-link>
+        <router-link to="/about">Administratör</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  setup(props) {
+    console.log(props);
+
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+.nav {
+  border-bottom: 1px solid $border--color;
+  box-shadow: $border--shadow;
+  position: fixed;
+  width: 100%;
+  background-color: $white;
+
+  &__inner {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    max-width: $grid-max-width;
+    width: 100%;
+    margin: 0 auto;
+    padding: 1rem 0.5rem;
+
+    &--right,
+    &--left {
+      a {
+        padding: 0.25rem 1rem;
+        font-size: 1.25rem;
+        color: $text-color;
+        text-decoration: none;
+
+        &.router-link-exact-active {
+          color: $text-color--active;
+        }
+
+        &:hover {
+          color: $text-color--hover;
+        }
+      }
+    }
+  }
+}
+</style>
